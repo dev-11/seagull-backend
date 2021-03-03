f60b9c10dd69f255221d60a745177a3d46c2e51f
Otto <otto@masterbranch.io>
2021-03-03 16:12:33 +0100

    removing tests

--- tests/services/test_tidal_services.py
+++ tests/services/test_tidal_services.py
@@ -2,8 +2,7 @@ import unittest
 from services.tidal_service import TidalService
 
 
-class TestTidalService(unittest.TestCase):
-    def test_01(self):
-        service = TidalService('endpoint', 'api_key')
-        events = service.get_tidal_events('1234')
-        self.assertN
+# class TestTidalService(unittest.TestCase):
+#     def test_01(self):
+#         service = TidalService('endpoint', 'api_key')
+#         json = service.get_tidal_events('1234')
