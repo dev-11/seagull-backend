f7beb005b660c7a5b098eb3831125dcd94caf00a
Otto <otto@masterbranch.io>
2021-03-04 16:08:37 +0100

    return None on unkwon location

--- services/station_id_service.py
+++ services/station_id_service.py
@@ -1,6 +1,6 @@
 class StationIdService:
     def get_station_id(self, location):
-        return station_ids[location]
+        return station_ids[location] if location in station_ids.keys() else None
 
 
 station_ids = {
