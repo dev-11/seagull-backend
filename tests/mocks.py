038bf0d4eeda9641fb1dd916a6736c8067ccd8ef
Otto <otto@masterbranch.io>
2021-03-05 13:24:36 +0100

    adding tidal service tests

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,7 +1,9 @@
+from datetime import timedelta
 from unittest.mock import Mock
 
 from dom.mappers import AdmiraltyEventTypeMapper, TideType
 from repositories import EnvironmentRepository
+from services.adjust_time_service import ChiswickEyotTimeService
 
 
 def get_mocked_AdmiraltyEventTypeMapper():
@@ -16,3 +18,10 @@ def get_env_repo():
     er.get_parameter = Mock(name='get_parameter')
     er.get_parameter.return_value = 'test_value'
     return er
+
+
+def get_mocked_adjust_time_service():
+    mocked = ChiswickEyotTimeService()
+    mocked.get_adjusted_time = Mock(name='get_adjusted_time')
+    mocked.get_adjusted_time.return_value = timedelta(minutes=0)
+    return mocked
--- tests/services/test_tidal_services.py
+++ tests/services/test_tidal_services.py
@@ -1,8 +1,26 @@
 import unittest
+from datetime import datetime
 
+from dom import TideEvent, TideType
 from services.tidal_service import TidalService
+from tests.mocks import get_mocked_adjust_time_service
 
-# class TestTidalService(unittest.TestCase):
-#     def test_01(self):
-#         service = TidalService('endpoint', 'api_key')
-#         json = service.get_tidal_events('1234')
+
+class TestTidalService(unittest.TestCase):
+    def test_get_tidal_data_raises_TypeError_for_None_data(self):
+        service = TidalService(get_mocked_adjust_time_service())
+        self.assertRaises(TypeError, service.get_tidal_data, None)
+
+    def test_get_tidal_data_returns_empty_array_for_empty_array(self):
+        service = TidalService(get_mocked_adjust_time_service())
+        result = service.get_tidal_data([])
+        self.assertEqual([], result)
+
+    def test_get_tidal_data_returns_correct_data(self):
+        service = TidalService(get_mocked_adjust_time_service())
+        result = service.get_tidal_data([TideEvent(type=TideType.High, time=datetime(1, 1, 1), height=1)])
+        self.assertEqual(1, len(result))
+        self.assertEqual(TideType.High, result[0].type)
+        self.assertEqual(datetime(1, 1, 1), result[0].time)
+        self.assertEqual(1, result[0].height)
+
